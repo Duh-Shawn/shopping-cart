d1598169121b3232de9de66470fdf08a4869b41a
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2023-03-01 12:42:04 -0500

    Fix router basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/app">
+    <BrowserRouter basename="/shopping-cart">
       <App />
     </BrowserRouter>
   </React.StrictMode>
